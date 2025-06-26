974c384454219f50564272fa394f23ca585f0725
sierram2 <sierram2@uthscsa.edu>
2025-06-26 16:49:17 -0500

    update PyScript output

--- script.js
+++ script.js
@@ -12,37 +12,6 @@ function createGlitterParticle() {
   glitter.style.setProperty('--size', (Math.random() * 3 + 2) + 'px');
   glitter.style.setProperty('--opacity', (Math.random() * 0.5 + 0.5));
 
-  const x = (Math.random() - 0.5) * 10 + 'px';
-  const y = - (Math.random() * 20 + 10) + 'px';
-
-  glitter.style.setProperty('--x', x);
-  glitter.style.setProperty('--y', y);
-
-  glitterName.appendChild(glitter);
-  glitter.addEventListener('animationend', () => glitter.remove());
-}
-
-glitterName.addEventListener('mouseenter', () => {
-  let count = 0;
-  const glitterBurst = setInterval(() => {
-    createGlitterParticle();
-    count++;
-    if (count > 20) clearInterval(glitterBurst);
-  }, 60);
-
-
-const glitterName = document.getElementById('glitter-name');
-
-function createGlitterParticle() {
-  const glitter = document.createElement('div');
-  glitter.classList.add('glitter-dot');
-
-  glitter.style.left = (Math.random() * glitterName.offsetWidth) + 'px';
-  glitter.style.top = (Math.random() * glitterName.offsetHeight * 0.6) + 'px';
-
-  glitter.style.setProperty('--size', (Math.random() * 3 + 2) + 'px');
-  glitter.style.setProperty('--opacity', (Math.random() * 0.5 + 0.5));
-
   const x = (Math.random() - 0.5) * 80 + 'px';
   const y = (Math.random() - 0.5) * 80 + 'px';
 
@@ -53,7 +22,17 @@ function createGlitterParticle() {
   glitter.addEventListener('animationend', () => glitter.remove());
 }
 
-// sparkle burst + redirect
+// Glitter on hover
+glitterName.addEventListener('mouseenter', () => {
+  let count = 0;
+  const glitterBurst = setInterval(() => {
+    createGlitterParticle();
+    count++;
+    if (count > 20) clearInterval(glitterBurst);
+  }, 60);
+});
+
+// Glitter burst + redirect on click
 glitterName.addEventListener('click', () => {
   let count = 0;
   const burst = setInterval(() => {
@@ -63,9 +42,7 @@ glitterName.addEventListener('click', () => {
       clearInterval(burst);
       setTimeout(() => {
         window.location.href = 'index.html';
-      }, 300); // short pause before redirect
+      }, 300);
     }
   }, 20);
-})
-
 });
